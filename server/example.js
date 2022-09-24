e5fec18a34c839c7ea2b4df6fbe7abae6e452f2d
cryptic <cryptic@pop-os.localdomain>
2022-09-24 18:43:46 +0100

    Change frontend

--- server/example.js
+++ server/example.js
@@ -1,16 +1,27 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-const fs = require('fs');
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('http://127.0.0.1:5173/', {
+		waitUntil: 'networkidle0'
+	});
+	const scrollDimension = await page.evaluate(() => {
+		return {
+			width: document.scrollingElement.scrollWidth,
+			height: document.scrollingElement.scrollHeight
+		};
+	});
+	await page.setViewport({
+		width: scrollDimension.width,
+		height: scrollDimension.height
+	});
+	await page.pdf({
+		path: 'example.pdf',
+		printBackground: true,
+		width: scrollDimension.width,
+		height: scrollDimension.height
+	});
 
-(async()=>{
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://127.0.0.1:5173/', {
-    waitUntil: 'networkidle0'
-  });
-  await page.pdf({path: 'example.pdf', format: 'a4'});
-
-  await browser.close();
-
-
+	await browser.close();
 })();
