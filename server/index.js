12267a44bb7b01b44588595d282383321f3af1f4
cryptic <cryptic@pop-os.localdomain>
2022-09-25 17:31:14 +0100

    Add more functionality and categorize

--- server/index.js
+++ server/index.js
@@ -10,24 +10,32 @@ app.use(cors());
 
 // Variables
 const port = process.env.PORT || 8000;
-const link = process.env.NODE_ENV==='development'?`http://localhost:${port}`:"";
-
-
+const link = process.env.NODE_ENV === 'development' ? `http://localhost:${port}` : '';
 
 // App routes
-app.get('/', (req,res)=>{
- //Converting data to JSON
-const workbook = XLSX.readFile('./Sample.xlsx');
-const sheet_name = workbook.SheetNames[0];
-const worksheet = workbook.Sheets[sheet_name];
-const jsa = XLSX.utils.sheet_to_json(worksheet);
-res.json(jsa);
-
+app.get('/', (req, res) => {
+	//Converting data to JSON
+	const workbook = XLSX.readFile('./Sample.xlsx');
+	const sheet_name = workbook.SheetNames[0];
+	const worksheet = workbook.Sheets[sheet_name];
+	const jsa = XLSX.utils.sheet_to_json(worksheet);
+	let uniqueCategories = [];
+	const result = jsa.reduce((categories, product) => {
+		const category = product['Category'];
+		if (categories[category] == undefined) {
+			categories[category] = [];
+			uniqueCategories.push(category);
+		}
+		categories[category].push(product);
+		return categories;
+	});
+	res.json({
+		data: result,
+		set: uniqueCategories
+	});
 });
 
-
 // Starting server
-app.listen(port, ()=>{
-  console.log(`App Started.. Click here to visit the app: ${link}`);
-} );
-
+app.listen(port, () => {
+	console.log(`App Started.. Click here to visit the app: ${link}`);
+});
